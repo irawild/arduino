08fc8a49a9a54d303caa32605ed483df2073cd50
irawild <irawildjunior@gmail.com>
2021-12-19 16:45:13 -0300

    Ajustado algoritmo de distância e gatilho

--- ping/Ping/Ping.ino
+++ ping/Ping/Ping.ino
@@ -1,42 +1,58 @@
+#include <Servo.h>
+
+Servo myServo;
+int MAX_VAL;
 const int trigPin = 12;
-const int echoPin = 10;
+const int echoPin = 11;
+int MIN_DISTANCE = 100;
  
 void setup() {
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  myServo.attach(10);
+  MAX_VAL = 180;
 }
  
 void loop()
 {
-long duration, inches, cm;
- 
-// The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
- 
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(5);
-digitalWrite(trigPin, LOW);
- 
-// The same pin is used to read the signal from the PING))): a HIGH
-// pulse whose duration is the time (in microseconds) from the sending
-// of the ping to the reception of its echo off of an object.
- 
-duration = pulseIn(echoPin, HIGH);
- 
-// convert the time into a distance
-inches = microsecondsToInches(duration);
-cm = microsecondsToCentimeters(duration);
- 
-Serial.print(inches);
-Serial.print("in, ");
-Serial.print(cm);
-Serial.print("cm");
-Serial.println();
- 
-delay(100);
+  long duration, inches, cm;
+      
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+   
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+   
+  duration = pulseIn(echoPin, HIGH);
+   
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+   
+  //Serial.print(inches);
+  //Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  if (cm < MIN_DISTANCE)
+    {
+      myServo.write(10);
+      //Serial.print("PWM 511");
+      //Serial.println();
+    }
+   else
+   {
+      myServo.write(150);
+      //Serial.print("PWM 0");
+      //Serial.println();
+   }
+   
+  delay(100);
 }
  
 long microsecondsToInches(long microseconds)
@@ -46,7 +62,7 @@ long microsecondsToInches(long microseconds)
 // second).  This gives the distance travelled by the ping, outbound
 // and return, so we divide by 2 to get the distance of the obstacle.
 // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-return microseconds / 74 / 2;
+  return microseconds / 74 / 2;
 }
  
 long microsecondsToCentimeters(long microseconds)
@@ -54,5 +70,5 @@ long microsecondsToCentimeters(long microseconds)
 // The speed of sound is 340 m/s or 29 microseconds per centimeter.
 // The ping travels out and back, so to find the distance of the
 // object we take half of the distance travelled.
-return microseconds / 29 / 2;
+  return microseconds / 29 / 2;
 }
